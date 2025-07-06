80706e6c6359544a89209a6d0a1912e2e63e2b24
cdandeniya <chanul.dandeniya@stonybrook.edu>
2025-07-06 14:33:39 -0400

    Add socket includes and port definitions

--- src/hw4.c
+++ src/hw4.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define PORT1 2201
+#define PORT2 2202
 
 int main() {
     printf("Battleship server starting...
 ");
+    printf("Will listen on ports %d and %d
+", PORT1, PORT2);
     return 0;
 }
