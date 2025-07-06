16d6404b61430df8590312223af38200084e9bf9
cdandeniya <chanul.dandeniya@stonybrook.edu>
2025-07-06 14:34:30 -0400

    Add socket creation for both players

--- src/hw4.c
+++ src/hw4.c
@@ -9,9 +9,23 @@
 #define PORT2 2202
 
 int main() {
-    printf("Battleship server starting...
+    int server_fd1, server_fd2;
+    struct sockaddr_in address1, address2;
+    int addrlen = sizeof(struct sockaddr_in);
+
+    // Create first socket
+    if ((server_fd1 = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Create second socket
+    if ((server_fd2 = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Created sockets for both players
 ");
-    printf("Will listen on ports %d and %d
-", PORT1, PORT2);
     return 0;
 }
